30f5219319e0404a32d5b94ea55e4efce759a419
MissTipo <dorine.a.tipo@gmail.com>
2023-01-17 06:01:13 +0300

    export to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Exports data in the JSON format"""
+import json
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = get(url + "users/{}".format(argv[1])).json()
+    user_name = user.get("user_name")
+    todos= get(url + "todos", params={"userId": argv[1]}).json()
+    with open("{}.json".format(argv[1]), "w", encoding="UTF8") as f:
+        json.dump({argv[1]: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "user_name": user_name
+            } for t in todos]}, f)
