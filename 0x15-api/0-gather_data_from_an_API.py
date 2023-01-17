4f8e98c669942a063b3cc89ed5049b9272499a6e
MissTipo <dorine.a.tipo@gmail.com>
2023-01-17 05:12:15 +0300

    add documentation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
-"""
-A script that returns information about an employee's ODO list progress
-using REST api
-"""
-from requests import get
-from sys import argv
+"""Uses https://jsonplaceholder.typicode.com"""
+import requests
+import sys
 
 
-if __name__ == '__main__':
-    """ensures code isn't executed when imported"""
-    req = get('https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
-    u = req.json()
-    user_name = u.get('name')
-    req = get(f'https://jsonplaceholder.typicode.com/todos',
-              params={"userId": argv[1]})
-    t = req.json()
-    # total number of tasks
-    total_tasks = len(t)
-    # completed tasks
-    completed = 0
-    completed_task = []
-    for task in t:
-        if task['completed']:
-            completed += 1
-            completed_task.append(task['title'])
-    response = f'Employee {user_name} is done with:'
-    'tasks ({completed}/{total_tasks})'
-    print(response)
-    for task in completed_task:
-        print(f'\t {task}')
+if __name__ == "__main__":
+    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                     .format(sys.argv[1]))
+    d = r.json()
+    name = d.get('name')
+    user_id = d.get('id')
+    r = requests.get('https://jsonplaceholder.typicode.com/todos')
+    d_todos = r.json()
+    tasks, completed = 0, 0
+    titles = []
+    for i in d_todos:
+        if i.get('userId') == user_id:
+            tasks += 1
+            if i.get('completed'):
+                completed += 1
+                titles.append(i.get('title'))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, completed, tasks))
+    for i in titles:
+        print('\t', i)
