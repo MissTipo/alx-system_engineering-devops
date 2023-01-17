121c6c99eeee8ea24eeb65022b6fdfb7f725ce77
MissTipo <dorine.a.tipo@gmail.com>
2023-01-17 06:02:54 +0300

    export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
     user = get(url + "users/{}".format(argv[1])).json()
     user_name = user.get("user_name")
-    todos= get(url + "todos", params={"userId": argv[1]}).json()
+    todos = get(url + "todos", params={"userId": argv[1]}).json()
     with open("{}.json".format(argv[1]), "w", encoding="UTF8") as f:
         json.dump({argv[1]: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "user_name": user_name
-            } for t in todos]}, f)
+            "task": t.get("title"),
+            "completed": t.get("completed"),
+            "user_name": user_name
+        } for t in todos]}, f)
