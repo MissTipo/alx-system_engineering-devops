575b6f0709a3013391514d9b737f2866e0262c41
MissTipo <dorine.a.tipo@gmail.com>
2023-01-17 04:51:57 +0300

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+A script that returns information about an employee's ODO list progress
+using REST api
+"""
+from requests import get
+from sys import argv
+
+
+if __name__ == '__main__':
+    req = get('https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+    u = req.json()
+    user_name = u.get('name')
+    req = get(f'https://jsonplaceholder.typicode.com/todos',
+              params={"userId": argv[1]})
+    t = req.json()
+    # total number of tasks
+    total_tasks = len(t)
+    # completed tasks
+    completed = 0
+    completed_task = []
+    for task in t:
+        if task['completed']:
+            completed += 1
+            completed_task.append(task['title'])
+    response = f'Employee {user_name} is done with'
+    'tasks ({completed}/{total_tasks}):'
+    print(response)
+    for task in completed_task:
+        print(f'\t {task}')
